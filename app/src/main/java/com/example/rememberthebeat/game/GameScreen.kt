87084b2b5888e8c078a70fb7dfca83e8325c7937
Samson Ha <whiteram81@gmail.com>
2023-08-25 23:50:17 -0400

    fixed audio playing when app in background, added recording click times and click counts.

--- app/src/main/java/com/example/rememberthebeat/game/GameScreen.kt
+++ app/src/main/java/com/example/rememberthebeat/game/GameScreen.kt
@@ -4,14 +4,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
@@ -27,23 +33,89 @@ fun GameScreen(
     ){
     val context = LocalContext.current
     val exoPlayer = remember { ExoPlayer.Builder(context).build() }
-
-    val audioSource = MediaItem.fromUri("android.resource://${context}/${R.raw.ogg120}")
+    val audioSource = MediaItem.fromUri("android.resource://${context}/${R.raw.ogg120trimmed}")
     exoPlayer.apply {
         setMediaItem(audioSource)
         repeatMode = Player.REPEAT_MODE_ONE
         prepare()
         play()
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {  }, modifier = Modifier.fillMaxSize(), shape = RectangleShape) {
+    val lifecycleEvent = rememberLifecycleEvent()
+    LaunchedEffect(lifecycleEvent) {
+        if (lifecycleEvent == Lifecycle.Event.ON_PAUSE) {
+            exoPlayer.pause()
+        }
+        else if (lifecycleEvent == Lifecycle.Event.ON_STOP) {
+            exoPlayer.pause()
         }
     }
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val clickCount by viewModel.clickCount.collectAsState()
+        val textT:String = when {
+            clickCount < 4 -> "tap along to the beat"
+            clickCount in 4..11 -> "that's great! now we'll see how well you can play on your own."
+            clickCount in 12..15 -> "the backing track is going to fade out"
+            clickCount == 16 -> "the backing track is going to fade out\n5"
+            clickCount == 17 -> "the backing track is going to fade out\n4"
+            clickCount == 18 -> "the backing track is going to fade out\n3"
+            clickCount == 19 -> "the backing track is going to fade out\n2"
+            clickCount == 20 -> "the backing track is going to fade out\n1"
+            clickCount == 35 -> "the backing track is going to fade out\n5"
+            clickCount == 36 -> "the backing track is going to fade out\n4"
+            clickCount == 37 -> "the backing track is going to fade out\n3"
+            clickCount == 38 -> "the backing track is going to fade out\n2"
+            clickCount == 39 -> "the backing track is going to fade out\n1"
+            else -> ""
+        }
+        val vol:Float = when {
+            clickCount < 16 -> 1F
+            clickCount == 16 -> 0.8F
+            clickCount == 17 -> 0.6F
+            clickCount == 18 -> 0.4F
+            clickCount == 19 -> 0.2F
+            else -> 0F
+        }
+
+        Button(
+            onClick = {
+                viewModel.incrementClickCount()
+                exoPlayer.volume = 1F//vol
+                if (clickCount==20){
+                    viewModel.setStart(System.currentTimeMillis())
+                }
+                if (clickCount in 21..40){
+                    viewModel.addClickTime(System.currentTimeMillis())
+                }
+            },
+            modifier = Modifier.fillMaxSize(),
+            shape = RectangleShape) {
+        }
+        Text(text = textT)
+        if (clickCount == 41){
+            viewModel.calculateScores()
+        }
+        if (clickCount > 39){
+            Text(text = viewModel.scores.toString())
+        }
+    }
+}
+
+@Composable
+fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current): Lifecycle.Event {
+    var state by remember { mutableStateOf(Lifecycle.Event.ON_ANY) }
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            state = event
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+    return state
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/rememberthebeat/game/GameViewModel.kt
+++ app/src/main/java/com/example/rememberthebeat/game/GameViewModel.kt
@@ -1,9 +1,38 @@
 package com.example.rememberthebeat.game
 
+import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.media3.exoplayer.ExoPlayer
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GameViewModel @Inject constructor(): ViewModel(){
+    private val _clickCount = MutableStateFlow(0)
+    val clickCount: StateFlow<Int> = _clickCount
+    fun incrementClickCount() {
+        viewModelScope.launch {
+            _clickCount.value++
+        }
+    }
+    var clickTimes = mutableListOf<Long>()
+    var scores = mutableListOf<Int>()
+    var correctTimes = mutableListOf<Long>()
+    fun addClickTime(time: Long){
+        clickTimes.add(time)
+    }
+    fun setStart(time: Long){
+        for (i in 1..20) {
+            correctTimes.add(time + i * 500)
+        }
+    }
+    fun calculateScores(){
+        for (i in 1 until clickTimes.size){
+            scores.add((clickTimes[i] - correctTimes[i]).toInt())
+        }
+    }
 }

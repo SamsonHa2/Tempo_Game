290e38d55fe0fa2822ac5fc42bb902e0dd9fb9e3
Samson Ha <whiteram81@gmail.com>
2023-08-31 10:01:00 -0400

    Changed play button triangle from text to icon

--- app/src/main/java/com/example/rememberthebeat/start/StartScreen.kt
+++ app/src/main/java/com/example/rememberthebeat/start/StartScreen.kt
@@ -4,20 +4,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.rememberthebeat.R
 import com.example.rememberthebeat.ui.theme.RememberTheBeatTheme
 
 @Composable
@@ -74,16 +75,14 @@ fun StartScreen(
                     .size(144.dp)
                     .align(CenterHorizontally)
             ) {
-                Column {
-                    Text(
-                        text = "▶",
-                        fontSize = 84.sp,
-                        textAlign = TextAlign.End
-                    )
-                    Spacer(modifier = Modifier.height(20.dp).weight(1F))
-                }
+                Icon(
+                    painter = painterResource(id = R.drawable.play_icon),
+                    contentDescription = "play icon",
+                    modifier = Modifier
+                        .size(75.dp)
+                        .padding(start=10.dp)
+                )
             }
-            Spacer(modifier = Modifier.height(100.dp))
         }
     }
 }

fc463af32a585ba80235d90241192e3fcd34c187
Samson Ha <whiteram81@gmail.com>
2023-09-05 16:44:38 -0400

    add falling shapes animation

--- app/src/main/java/com/example/rememberthebeat/game/GameScreen.kt
+++ app/src/main/java/com/example/rememberthebeat/game/GameScreen.kt
@@ -1,6 +1,10 @@
 package com.example.rememberthebeat.game
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,15 +15,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
@@ -37,7 +43,11 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.rememberthebeat.ui.theme.RememberTheBeatTheme
 import com.example.rememberthebeat.R
+import com.example.rememberthebeat.data.models.Shape
 import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
 
 @Composable
 fun GameScreen(
@@ -77,19 +87,46 @@ fun GameScreen(
             modifier = Modifier
                 .fillMaxSize()
         ) {
-
-            Button(
-                onClick = {
+            var shapes by remember { mutableStateOf(emptyList<Shape>()) }
+            val shapeList = listOf("circle", "triangle", "square", "pentagon", "hexagon")
+            val colorList = listOf(Color.Blue, Color.Magenta, Color.Yellow, Color.Green, Color.Red)
+            val horizontalOffset =  Random.nextFloat() * 1000
+            val verticalOffset =  Random.nextFloat() * 2000
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Cyan)
+                    .clickable {
+                        shapes += Shape(
+                            type = shapeList[Random.nextInt(0, shapeList.size)],
+                            horizontalOffset = horizontalOffset,
+                            verticalOffset = verticalOffset,
+                            color = colorList[Random.nextInt(0, colorList.size)]
+                        )
                     viewModel.incrementClickCount()
                     viewModel.updateGameState()
                     exoPlayer.volume = viewModel.getGameVolume()
-                },
-                modifier = Modifier.fillMaxSize(),
-                shape = RectangleShape,
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan)
+                }
             ) {
-                Text(text = viewModel.getGameText()+"\n\n\n\n\n\n\n\n\n\n", fontSize = 24.sp, lineHeight = 28.sp, textAlign = TextAlign.Center)
-                Spacer(modifier = Modifier.height(30.dp))
+                for (shape in shapes) {
+                    when (shape.type) {
+                        "circle" -> DrawCircleShape(shape)
+                        "triangle" -> DrawTriangleShape(shape)
+                        "square" -> DrawSquareShape(shape)
+                        "pentagon" -> DrawPentagonShape(shape)
+                        "hexagon" -> DrawHexagonShape(shape)
+                    }
+                }
+                Text(
+                    text = viewModel.getGameText()+"\n\n\n\n\n\n\n\n\n\n",
+                    color = Color.Black,
+                    fontSize = 24.sp,
+                    lineHeight = 28.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 20.dp, vertical = 48.dp)
+                )
             }
         }
     }
@@ -101,7 +138,6 @@ fun GameScreen(
             navController = navController
         )
     }
-
 }
 
 @Composable
@@ -241,7 +277,6 @@ fun Summary(
         }
     }
 }
-
 @Composable
 fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current): Lifecycle.Event {
     var state by remember { mutableStateOf(Lifecycle.Event.ON_ANY) }
@@ -257,6 +292,156 @@ fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.
     return state
 }
 
+@Composable
+private fun DrawCircleShape(circle:Shape) {
+    var updatedVisible by remember { mutableStateOf(circle.visible) }
+
+    val shapeSize by animateFloatAsState(
+        targetValue = if (updatedVisible) 0F else 1F, label = "",
+        animationSpec = tween(1000)
+    )
+    LaunchedEffect(circle) {
+        updatedVisible = true
+    }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .scale(shapeSize)
+    ) {
+        drawCircle(
+            color = circle.color,
+            center = Offset(circle.horizontalOffset, circle.verticalOffset),
+            radius = 450F / 2
+        )
+    }
+}
+
+@Composable
+private fun DrawTriangleShape(triangle:Shape) {
+    var updatedVisible by remember { mutableStateOf(triangle.visible) }
+
+    val shapeSize by animateFloatAsState(
+        targetValue = if (updatedVisible) 0F else 1F, label = "",
+        animationSpec = tween(1000)
+    )
+
+    val size = 450F * shapeSize
+
+    LaunchedEffect(triangle) {
+        updatedVisible = true
+    }
+    val trianglePath = Path().apply {
+        // Moves to top center position
+        moveTo(size / 2f + triangle.horizontalOffset, triangle.verticalOffset)
+        // Add line to bottom right corner
+        lineTo(size + triangle.horizontalOffset, size + triangle.verticalOffset)
+        // Add line to bottom left corner
+        lineTo(triangle.horizontalOffset, size +  triangle.verticalOffset)
+    }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .scale(shapeSize)
+    ) {
+        drawPath(path = trianglePath, color = triangle.color)
+    }
+}
+
+@Composable
+private fun DrawSquareShape(square:Shape) {
+    var updatedVisible by remember { mutableStateOf(square.visible) }
+
+    val shapeSize by animateFloatAsState(
+        targetValue = if (updatedVisible) 0F else 1F, label = "",
+        animationSpec = tween(1000)
+    )
+    LaunchedEffect(square) {
+        updatedVisible = true
+    }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .scale(shapeSize)
+    ) {
+        drawRect(
+            color = square.color,
+            topLeft = Offset(square.horizontalOffset, square.verticalOffset),
+            size = Size(450F, 450F)
+        )
+    }
+}
+
+@Composable
+private fun DrawPentagonShape(pentagon:Shape) {
+    var updatedVisible by remember { mutableStateOf(pentagon.visible) }
+
+    val shapeSize by animateFloatAsState(
+        targetValue = if (updatedVisible) 0F else 1F, label = "",
+        animationSpec = tween(1000)
+    )
+
+    val size = 450F * shapeSize / 2
+    LaunchedEffect(pentagon) {
+        updatedVisible = true
+    }
+    val pentagonPath = Path().apply {
+        // Moves to top center position
+        moveTo(
+            x = size + pentagon.horizontalOffset + (size * cos(0.0)).toFloat(),
+            y = size + pentagon.verticalOffset + (size * sin(0.0)).toFloat()
+        )
+        for (i in 1..4) {
+            val angle = 2.0 * Math.PI / 5
+            lineTo(
+                x = size + pentagon.horizontalOffset + (size * cos(angle * i)).toFloat(),
+                y = size + pentagon.verticalOffset + (size * sin(angle * i)).toFloat()
+            )
+        }
+    }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .scale(shapeSize)
+    ) {
+        drawPath(path = pentagonPath, color = pentagon.color)
+    }
+}
+
+@Composable
+private fun DrawHexagonShape(hexagon: Shape) {
+    var updatedVisible by remember { mutableStateOf(hexagon.visible) }
+
+    val shapeSize by animateFloatAsState(
+        targetValue = if (updatedVisible) 0F else 1F, label = "",
+        animationSpec = tween(1000)
+    )
+
+    val size = 450F * shapeSize / 2
+    LaunchedEffect(hexagon) {
+        updatedVisible = true
+    }
+    val hexagonPath = Path().apply {
+        // Moves to top center position
+        moveTo(
+            x = size + hexagon.horizontalOffset + (size * cos(0.0)).toFloat(),
+            y = size + hexagon.verticalOffset + (size * sin(0.0)).toFloat()
+        )
+        for (i in 1..5) {
+            val angle = 2.0 * Math.PI / 6
+            lineTo(
+                x = size + hexagon.horizontalOffset + (size * cos(angle * i)).toFloat(),
+                y = size + hexagon.verticalOffset + (size * sin(angle * i)).toFloat()
+            )
+        }
+    }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .scale(shapeSize)
+    ) {
+        drawPath(path = hexagonPath, color = hexagon.color)
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GamePreview() {
